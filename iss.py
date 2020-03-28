bba7df4cd3a3e0daf2377c052d5242c3b692108b
Tiffany McLean <tamclean324@gmail.com>
2020-03-27 22:49:39 -0400

    creates a list of the astronauts, their spacecrafts, and the total number of astronauts

--- iss.py
+++ iss.py
@@ -1,6 +1,28 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-__author__ = '???'
+__author__ = 'Tiffany McLean'
+
+import requests
+
+
+def get_astronauts_info():
+    """
+    Get a list of the astronatuts currently in space.  Include their names, the spacecraft that they are currently on board, and the total number of astronatuts in space
+    """
+
+    r = requests.get('http://api.open-notify.org/astros.json').json()
+    number_of_astronauts = r['number']
+    astronauts = r['people']
+
+    for hero in astronauts:
+        name = hero['name']
+        spacecraft = hero['craft']
+        print(f'Name: {name}, Spacecraft: {spacecraft}')
+    print(f'Total Number of Astronauts: {number_of_astronauts}')
+
+
+    # return r
+get_astronauts_info()
 
 
 def main():
