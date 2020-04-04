8c8b578852522254637a71b423dfd95fc03ae6c2
Tiffany McLean <tamclean324@gmail.com>
2020-04-03 20:37:39 -0400

    tracks ISS current position and the next time it will pass over Indianapolis

--- iss.py
+++ iss.py
@@ -1,45 +1,108 @@
 #!/usr/bin/env python3
 
-__author__ = 'Tiffany McLean'
+__author__ = "Tiffany McLean with the help of Piero and Daniel's demo"
 
 import requests
+import time
+import turtle
+
+
+iss_icon = 'iss.gif'
+world_map = 'map.gif'
+base_url = 'http://api.open-notify.org'
 
 
 def get_astronauts_info():
     """
     Get a list of the astronatuts currently in space.  Include their names, the spacecraft that they are currently on board, and the total number of astronatuts in space
+
+    Return a dict of current astronauts and their spacecrafts
     """
-
-    r = requests.get('http://api.open-notify.org/astros.json').json()
-    number_of_astronauts = r['number']
-    astronauts = r['people']
-
-    for hero in astronauts:
-        name = hero['name']
-        spacecraft = hero['craft']
-        print(f'Name: {name}, Spacecraft: {spacecraft}')
-    print(f'Total Number of Astronauts: {number_of_astronauts}')
-    return r
+    r = requests.get(base_url + '/astros.json')
+    r.raise_for_status()
+    return r.json()['people']
 
 
 def locate_iss_spacestation():
     """
     Get the current geographic coordinates (lat/lon) of the space station, along with a timestamp.
+    Get ISS position (lon, lat) as a float tuple
     """
-    r = requests.get('http://api.open-notify.org/iss-now.json').json()
-    lon = r["iss_position"]["longitude"]
-    lat = r["iss_position"]['latitude']
-    time = r['timestamp']
+    r = requests.get(base_url + '/iss-now.json')
+    # handles response from server just incase you get anything other than a 200 response
+    r.raise_for_status()
+    position = r.json()['iss_position']
+    lat = float(position['latitude'])
+    lon = float(position['longitude'])
+    return lat, lon
 
-    print(
-        f'The spacestation is currently located at the following coordinates: {lat}, {lon}.')
-    print(f'The current timestamp is: {time}')
-    return lon, lat, time
+
+def map_iss(lat, lon):
+    """
+    Draw a world map and place ISS icon at lat,lon
+    """
+    screen = turtle.Screen()
+    screen.setup(720, 360)
+    screen.bgpic(world_map)
+    screen.setworldcoordinates(-180, -90, 180, 90)
+
+    screen.register_shape(iss_icon)
+    iss = turtle.Turtle()
+    iss.shape(iss_icon)
+    iss.setheading(90)
+    iss.penup()
+    iss.goto(lon, lat)  # note the lon and lat are in reverse order here
+    return screen
+
+
+def compute_rise_time(lat, lon):
+    """
+    Return the next horizon rise-time of ISS for specific lat/lon
+    """
+    params = {'lat': lat, 'lon': lon}
+    r = requests.get(base_url + '/iss-pass.json', params=params)
+    r.raise_for_status()
+
+    passover_time = r.json()['response'][1]['risetime']
+    return time.ctime(passover_time)
 
 
 def main():
-    get_astronauts_info()
-    locate_iss_spacestation()
+    # Part A - get the astronauts and their crafts
+    astro_dict = get_astronauts_info()
+    print('\nCurrent people in space: {}'.format(len(astro_dict)))
+    for a in astro_dict:
+        print(' - {} in {}'.format(a['name'], a['craft']))
+
+    # Part B - get current position of ISS
+    lat, lon = locate_iss_spacestation()
+    print(
+        '\nCurrent ISS coordinates: lat={:.02f} lon={:.02f}'.format(lat, lon))
+
+    # Part C - Render ISS on world map
+    screen = None
+    try:
+        # Attempts to show turtle
+        screen = map_iss(lat, lon)
+
+        # Part D - Compute the next pass-over time for Indianapolis, IN
+        indy_lat = 39.768403
+        indy_lon = -86.158068
+        location = turtle.Turtle()
+        location.penup()
+        location.color('yellow')
+        location.goto(indy_lon, indy_lat)
+        location.dot(5)
+        location.hideturtle()
+        next_pass = compute_rise_time(indy_lat, indy_lon)
+        location.write(next_pass, align='center', font=('Arial', 12, 'normal'))
+    except RuntimeError as e:
+        print('ERROR: problem loading graphics: ' + str(e))
+
+    # leave screen open until user clicks on it
+    if screen is not None:
+        print('Click on screen to exit...')
+        screen.exitonclick()
 
 
 if __name__ == '__main__':
